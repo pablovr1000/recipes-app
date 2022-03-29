3566771e2a3d92db10f521397dcc73146bae1354
pablovr1000 <pablovr200@hotmail.com>
2022-03-29 17:42:52 -0300

    Requisito 05

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const validateLogin = () => {
+      const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+      const PASSWORD_MIN_LENGTH = 6;
+      const isEmailValid = emailRegex.test(email);
+      const isPasswordValid = password.length > PASSWORD_MIN_LENGTH;
+      console.log('Checando');
+      if (isEmailValid && isPasswordValid) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+    };
+
+    validateLogin();
+  }, [email, password]);
+
   return (
     <div className="containerLogin">
       <form className="formLogin">
@@ -12,6 +33,7 @@ export default function Login() {
           data-testid="email-input"
           placeholder="email"
           value={ email }
+          onChange={ ({ target: { value } }) => setEmail(value) }
         />
         <input
           className="inputPassword"
@@ -19,11 +41,13 @@ export default function Login() {
           type="password"
           data-testid="password-input"
           value={ password }
+          onChange={ ({ target: { value } }) => setPassword(value) }
         />
         <button
           className="buttonLogin"
           data-testid="login-submit-btn"
           type="button"
+          disabled={ isDisabled }
         >
           Enter
         </button>
