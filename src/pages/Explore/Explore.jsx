74098deeaebcaa38641cdd7424fd0ec8efdb0b1d
ACarlosMachado <antoniocarlos_acrm@hotmail.com>
2022-03-31 18:04:54 -0300

    feat: requisitos: 67 a 69

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 
 export default function Explore() {
   return (
     <>
       <Header />
-      <div>Explore</div>
+      <Link to="/explore/foods">
+        <button type="button" data-testid="explore-foods">Explore Foods</button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button type="button" data-testid="explore-drinks">Explore Drinks</button>
+      </Link>
     </>
   );
 }
