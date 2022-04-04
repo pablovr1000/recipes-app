ee477ab4ecfff0e0ddf38ca7d0ddae0a575a63b2
pablovr1000 <pablovr200@hotmail.com>
2022-04-04 15:53:01 -0300

    feat: set email user from localStorage on screen

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 
 export default function Profile() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const profileStorage = JSON.parse(localStorage.getItem('user'));
+    setUserData(profileStorage);
+  }, []);
+
   return (
-    <>
+    <div>
       <Header />
-      <div>
-        <h1 data-testid="profile-email">Email</h1>
-        <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-        <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
-        <button data-testid="profile-logout-btn" type="button">Logout</button>
-      </div>
-    </>
+      <h1 data-testid="profile-email">{userData.email}</h1>
+      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+      <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn" type="button">Logout</button>
+    </div>
   );
 }
