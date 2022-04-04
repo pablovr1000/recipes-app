5d3ba8c8fa8e8f0f6e149c093db5543b365a2e4d
pablovr1000 <pablovr200@hotmail.com>
2022-04-04 15:41:37 -0300

    feat: Set elements profile on screen

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,7 +5,12 @@ export default function Profile() {
   return (
     <>
       <Header />
-      <div>Profile</div>
+      <div>
+        <h1 data-testid="profile-email">Email</h1>
+        <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+        <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn" type="button">Logout</button>
+      </div>
     </>
   );
 }
