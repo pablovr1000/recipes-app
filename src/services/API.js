cd740cf3179d5420c8ac0924e226f6324dec94d6
Laura Lima <lauraolima2000@gmail.com>
2022-04-07 04:15:06 -0300

    fix: no lint errors

--- src/services/API.js
+++ src/services/API.js
@@ -30,20 +30,6 @@ export const getDrinks = async (search, option) => {
   return drinks;
 };
 
-export const getFoodByCategory = async (category) => {
-  const CATEGORIES_MAX_LENGTH = 12;
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
-  const { meals } = await response.json();
-  return Object.values(meals).splice(0, CATEGORIES_MAX_LENGTH);
-};
-
-export const getDrinkByCategory = async (category) => {
-  const CATEGORIES_MAX_LENGTH = 12;
-  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
-  const { drinks } = await response.json();
-  return Object.values(drinks).splice(0, CATEGORIES_MAX_LENGTH);
-};
-
 export const getFoodDetails = async (id) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   const { meals } = await response.json();
